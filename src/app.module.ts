d7f9633bcb27c05ef59b62cfce31e29537f27196
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-01-09 13:54:49 +0100

    initialize project

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
